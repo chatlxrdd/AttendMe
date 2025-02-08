fe52a40ae7bf61ce2e62f10392a63f6a955bfb29
chatlxrdd <londoyt@gmail.com>
2025-02-08 02:05:49 +0100

    Panel Logowania wykladowcy

--- AttendMe/src/App.vue
+++ AttendMe/src/App.vue
@@ -1,47 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="XDDDDDD" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <!-- Włączenie komponentu logowania -->
+    <LoginWykladowcy />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import LoginWykladowcy from './components/LoginWykladowcy.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+export default {
+  name: 'App',
+  components: {
+    LoginWykladowcy
   }
+};
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+/* Globalne style aplikacji */
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
 }
 </style>
