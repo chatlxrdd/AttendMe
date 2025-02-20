2989f6ea12e291d4b1b1508b343a15eb78c7fbf4
Aladynms <salamichal99@gmail.com>
2025-02-20 18:22:44 +0100

    exit3

---
+++ AttendMe/src/views/Scanner.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const tokenScanner = ref(route.params.tokenScanner as string);
+
+console.log(tokenScanner.value)
+
+
+</script>
+<template>
+    <h1>Skaner KURWA</h1>
+</template>
